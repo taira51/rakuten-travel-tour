8037db6ac2e4a19d4f54e095276da651f87b026b
大脇泰良 <b3256@oic.jp>
2018-08-14 22:06:36 +0900

    アンダーラインを付加するディレクティブ作成

---
+++ src/app/common/directive/underline.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnderline]'
+})
+export class UnderlineDirective implements OnInit {
+
+  constructor(private ElementRef: ElementRef) { }
+
+  ngOnInit() {
+    const element = this.ElementRef.nativeElement as HTMLElement;
+    element.style.textDecoration = 'underline';
+  }
+}
