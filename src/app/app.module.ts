7c520d33b318d9e442a758b5e0ffb017a18f9ef8
大脇泰良 <b3256@oic.jp>
2018-08-14 22:06:09 +0900

    共通設定変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ToDoListPageComponent } from './todo-list-page/todo-list-page.component';
+import { UnderlineDirective } from './common/directive/underline.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoListPageComponent
+    ToDoListPageComponent,
+    UnderlineDirective
   ],
   imports: [
     BrowserModule,
